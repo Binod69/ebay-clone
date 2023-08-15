e555d580d301e72ed9c6f0cfc328671fac9ae9df
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-15 15:06:08 +0545

    finished checkout

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,5 +1,7 @@
+import Image from 'next/image';
 import CheckoutItems from '../Components/CheckoutItems/CheckoutItems';
 import nepal from '../../public/images/nepal.webp';
+import logo from '../../public/images/logo.svg';
 const page = () => {
   const product = {
     id: 1,
@@ -30,6 +32,47 @@ const page = () => {
             </div>
             <div id="Items" className="bg-white rounded-lg mt-4">
               <CheckoutItems key={product.id} product={product} />
+            </div>
+          </div>
+          <div
+            id="PlaceOrder"
+            className="relative -top-[6px] w-[35%] border rounded-lg"
+          >
+            <div className="p-4">
+              <div className="flex items-baseline justify-between text-sm mb-1">
+                <div>Items: (3)</div>
+                <div>रु‎12.99</div>
+              </div>
+              <div className="flex items-baseline justify-between text-sm mb-1">
+                <div>Shipping</div>
+                <div>Free</div>
+              </div>
+              <div className="border-t" />
+              <div className="flex items-center justify-between my-4">
+                <div className="font-semibold">Order total</div>
+                <div className="text-2xl font-semibold">रु‎12.99</div>
+              </div>
+              <form>
+                <div
+                  className="border border-gray-500 p-2 rounded-sm"
+                  id="card-element"
+                />
+                <p
+                  id="card-error"
+                  role="alert"
+                  className="text-red-700 text-center font-semibold relative top-2"
+                />
+                <button
+                  type="submit"
+                  className="mt-4 bg-blue-600 text-lg w-full text-white font-semibold p-3 rounded-full"
+                >
+                  Confirm & Pay
+                </button>
+              </form>
+            </div>
+            <div className="flex items-center p-4 justify-center gap-2 border-t">
+              <Image src={logo} alt="logo.svg" width={50} />
+              <div className="font-light mb-2 mt-2">Money Back Guarantee</div>
             </div>
           </div>
         </div>
