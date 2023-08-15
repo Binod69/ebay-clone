65bc7ecbf8b7e5976fb5e4a38447ae8f68860e18
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-15 18:21:24 +0545

    created route

--- app/auth/page.js
+++ app/auth/page.js
@@ -4,7 +4,7 @@ import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
 import Link from 'next/link';
 const page = () => {
-  //   const supabase = createClientComponentClient();
+  const supabase = createClientComponentClient();
   return (
     <>
       <div id="AuthPage" className="w-full min-h-screen bg-white">
@@ -15,13 +15,13 @@ const page = () => {
           Login / Register
         </div>
         <div className="max-w-[400px] mx-auto px-2">
-          {/* <Auth
+          <Auth
             onlyThirdPartyProviders
-            redirectTo={`${windows.location.origin}/auth/callback`}
+            redirectTo={`${window.location.origin}/auth/callback`}
             supabaseClient={supabase}
             providers={['google']}
             appearance={{ theme: ThemeSupa }}
-          /> */}
+          />
         </div>
       </div>
     </>
