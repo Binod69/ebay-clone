2e54fb6f78f649ca013df1e2a2e43dd91abd8885
Binod Budhathoki <100508632+Binod69@users.noreply.github.com>
2023-08-15 13:31:59 +0545

    added product

--- app/page.js
+++ app/page.js
@@ -1,11 +1,37 @@
 'use client';
-import Carousel from './Components/Carousel/Carousel';
 import MainLayout from './Layouts/MainLayout';
+import Carousel from './Components/Carousel/Carousel';
+import Products from './Components/Products/Products';
+import nepal from '../public/images/nepal.webp';
 export default function Home() {
+  const products = [
+    {
+      id: 1,
+      title: 'Brown Laptop',
+      description: 'desc',
+      url: nepal,
+      price: 2000, //25.00
+    },
+    {
+      id: 2,
+      title: 'Brown Laptop 2',
+      description: 'desc',
+      url: nepal,
+      price: 2400,
+    },
+  ];
   return (
     <>
       <MainLayout>
         <Carousel />
+        <div className="max-w-[1200px] mx-auto">
+          <div className="text-2xl font-bold mt-4 mb-6 px-4">Products</div>
+          <div className="grid grid-cols-5 gap-4">
+            {products.map((product) => (
+              <Products key={product.id} product={product} />
+            ))}
+          </div>
+        </div>
       </MainLayout>
     </>
   );
